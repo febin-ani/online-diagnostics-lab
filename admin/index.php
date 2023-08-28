56d2596680ffb454044d8b077504b3476ebe7c60
Febin Ani <feb.in1434@gmail.com>
2023-08-28 10:15:04 +0530

    save

--- admin/index.php
+++ admin/index.php
@@ -30,7 +30,7 @@
                     <?php 
                         include('constant/config.php');
 
-                        $sql = "SELECT aid FROM tb_aregister ORDER BY aid";
+                        $sql = "SELECT aid FROM tb_aregister WHERE usertype = 'admin' ORDER BY aid";
                         $query = mysqli_query($conn,$sql);
 
                         $row = mysqli_num_rows($query);
@@ -52,7 +52,7 @@
                     <?php 
                         include('constant/config.php');
 
-                        $sql = "SELECT aid FROM tb_aregister ORDER BY aid";
+                        $sql = "SELECT aid FROM tb_aregister WHERE usertype = 'user' ORDER BY aid";
 
                         $query = mysqli_query($conn,$sql);
 
